906bd6f5b6c688be64f211c692b3f8708a854064
Val <vgeyvandov@gmail.com>
2016-06-10 15:34:50 -0400

    updating favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
-app.use(favicon());
+app.use(favicon(__dirname + '/web-cats.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
